48f979b270702f2dc4334ed3b0487f2673a7edf1
Uladzislau Matulis <quarzzap@gmail.com>
2016-11-07 16:30:50 +0300

    gamma.rb created but not finished

---
+++ sasm/3/gamma.rb
@@ -0,0 +1,16 @@
+require_relative "constants.rb"
+
+class Gamma
+    attr_reader :function
+
+    def initialize(shape = SHAPE, scale = SCALE)
+        @shape = @shape
+        @scale = @scale
+        @function = lambda do |scale, shape|
+
+        end
+    end
+
+    def next
+    end
+end
